51b4d1a5ea63abee98d5874bdaca09f9a677ff96
ClusivePlayz <48451500+ClusivePlayz@users.noreply.github.com>
2019-04-05 00:27:53 +0300

    Update DM.js

--- commands/DM.js
+++ commands/DM.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
     await.delete();
-    if(message.author.id !== 'uridherenoob') return;
+    if(message.author.id !== '548913423346434060') return;
            message.guild.members.forEach( member => {
                member.send(message.content.substr(3));
                message.delete();
