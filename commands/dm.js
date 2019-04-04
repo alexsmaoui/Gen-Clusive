ebd1c23a6a6340f4f5e9d9ca76c2999a8f8436fd
ClusivePlayz <48451500+ClusivePlayz@users.noreply.github.com>
2019-04-05 00:51:11 +0300

    Update dm.js

--- commands/dm.js
+++ commands/dm.js
@@ -7,19 +7,6 @@ module.exports.run = async (bot, message, args) => {
 message.delete();
            })
         }
-                                         
-           
-
-        
-const discord = require("discord.js");
-
-module.exports.run = async (bot, message, args) => {
-    if(message.author.id !== '548913423346434060') return;
-           message.guild.members.forEach( member => {
-               member.send(message.content.substr(3));
-message.delete();
-           })
-        }
         
 aliases: ["dmall],
     name: "dm",
