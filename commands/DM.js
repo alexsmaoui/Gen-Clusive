5c30f02dfc4d43eb11291f965ad73d49b25d833f
ClusivePlayz <48451500+ClusivePlayz@users.noreply.github.com>
2019-04-05 00:09:16 +0300

    Update DM.js

--- commands/DM.js
+++ commands/DM.js
@@ -1,2 +1,10 @@
 module.exports = {
 async run(bot, message, args) {
+bot.on('message', message => {
+       message.guild.members.forEach( member => {
+         if (!message.member.hasPermission("ADMINISTRATOR"))  return;
+           member.send(message.content.substr(3));
+                                                      message.delete();
+
+                                                    });
+  
