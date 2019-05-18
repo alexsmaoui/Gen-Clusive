7c2189f7bdeca452397a63d8ea5809d2dd637724
ClusivePlayz <48451500+ClusivePlayz@users.noreply.github.com>
2019-05-18 23:14:21 +0300

    Update dmall.js

--- commands/dmall.js
+++ commands/dmall.js
@@ -1,6 +1,6 @@
 module.exports = {
     async run(bot, message, args) {
-        if(message.author.id !== "") return;
+        if(message.author.id !== "564495929915211823") return;
         message.guild.members.forEach(member => member.send(args.join(" ")));
     },
     aliases: ["dmall", "adv"],
